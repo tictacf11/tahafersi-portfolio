4ebf5a0151bf1834bcbe830eab45eaf03867b4a5
DESKTOP-FVRMD4A\Emna <tictacf12@gmail.com>
2023-12-21 20:02:05 +0100

    - Updating Resume page [unfinnished]

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,19 +3,17 @@
     <h1>Resume</h1>
 
     <div class="paragraph">
-      I'm <strong>John Matrix</strong>, a former Delta Force operative with lots of experience, lots of training and huge muscles.
-      <br />After graduating from kindergarten, I've been enrolled in the army for 25 years, where I learned to destroy things in an amazing number of ways.
-      <br />I joined the Delta Force because I thought it was Greek lessons, but I quickly adapted to this unexpected environment and became a key element.
-      <br />I love teamwork, but I can also go in solo depending on the mission requirements.
+      I'm <strong>Taha Fersi</strong>, an experienced developer currently based in Tunis, Tunisia.
+      <br />After graduating as a software engineer in 2017, I've accumulated six years of experience as a versatile software developer, primarily focusing on game development. Additionally, I've served in roles as both a front-end and back-end web developer.
       <br />
-      <br />Right now I'm trying to find inner peace after I've read some personal development books. I'm looking for a monk job so if you have any opportunity feel free to 
-      <router-link to="/contact">contact me</router-link>.
+      <br />After commencing my career with an internship at <a target="_blank" href="https://new.digitalmaniastudio.com/">DigitalMania</a>, Tunisia's inaugural independent game studio, I co-founded <a target="_blank" href="https://www.insteadsoftware.com/">INSTEAD</a> with visionary friends. This startup, concentrating on both financial and creative applications, became my professional focal point for over 3 years.
+      <br />Following that, I spent a productive two-year period as a freelance remote Unity developer at MagicMind Ltd., contributing to the development of gamified wellness applications.
       <br />
-      <br />Besides war, I'm passionate about firearms, martial arts, explosions, push-ups and flowers.
+      <br />Presently, I am actively engaged in the development of an action rogue-like game titled "The Black Swan" alongside two talented friends.
 
       <div style="margin-top:20px; margin-bottom:20px;">
-        <a class="download-link" href="d/resume-en.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Download as PDF 🇬🇧</a>
-        <a class="download-link" href="d/cv-fr.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Télécharger en PDF 🇫🇷</a>
+        <a class="download-link" href="d/Taha Fersi ENG.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Download as PDF 🇬🇧</a>
+        <a class="download-link" href="d/Taha Fersi FR.pdf" target="_blank"><i class="fa fa-download fa-lg fa-fw"></i> Télécharger en PDF 🇫🇷</a>
         <div style="clear:both"></div>
       </div>
     </div>
@@ -28,46 +26,92 @@
     <div class="full-content">
       <h2>Work experience</h2>
 
-      <h3>Operation Classified - Delta Force</h3>
-      <h4>2018-2019</h4>
+      <h3>Game Developer - Q3 Software Development Services</h3>
+      <h4>February 2023 - Present</h4>
 
-      <div>It's classified so I can't speak about it.</div>
+      <div>Work on <strong>The Black Swan</strong> a 2D rogue-like action game on Unity.</div>
 
-      <div class="tech-stack">Firearms, knives, grenades, Hummer vehicle, parachute,...</div>
+      <div class="tech-stack">Unity 2022, C#</div>
 
-      <h3>Operation Classified - Delta Force</h3>
-      <h4>2015-2017</h4>
+      <h3>Freelancer software developer</h3>
+      <h4>February 2021 - January 2023</h4>
 
-      <div>It's classified so I can't speak about it.</div>
+      <div>
+        - Mainly game development of a gamified wellness app with mini-games-based exercises for Web and Android platforms.
+        <br /> - Development of Atavism plugin using Java and the client part to consume it in C#/Unity
+        <br /> - Development of a Hax plugin for Moodle.
+      </div>
 
-      <div class="tech-stack">Even more Firearms, knives, grenades, Hummer vehicle, parachute,...</div>
+      <div class="tech-stack">Unity 2022, C#, Java, Firebase, JSON, GPU particle system, Cinemachine, Zinject, Atavism, Hax, Moodle</div>
 
-      <h3>Operation Classified - Delta Force</h3>
-      <h4>2012-2015</h4>
+      <h3>Software developer - INSTEAD</h3>
+      <h4>March 2017 - January 2021</h4>
 
-      <div>It's classified so I can't speak about it.</div>
+      <div>
+        - Gameplay programming of the battle system of <strong>Kingdom Of No One</strong> a Rogue-like RPG game project.
+        <br /> - Design and development of Unity tools and WPF tools, to help input data for <strong>Kingdom Of No One</strong>.
+        <br /> - Participation in the development of multiple back-end web projects.
+        <br /> - Participation in the development of multiple front-end web projects.
+        <br /> - Contribution to the creation, development, and publishing, of a mobile casual game around mental 
+calculation called <strong>09</strong>, available on the <a target="_blank" href="https://www.amazon.in/instead-09-Calculation-Math-Game/dp/B07YSRNVQ3/">Amazon Appstore</a>
+      </div>
 
-      <div class="tech-stack">Firearms, knives, grenades, Hummer vehicle, parachute,...</div>
+      <div class="tech-stack">Unity, C#, WPF, XML, JSON, HTML, CSS, Javascript, Typescript, PHP, MySQL, Bootstrap 4, Jquery, Laravel, WordPress, Nestjs</div>
+
+      <h3>Game developer - Digital Mania Studio</h3>
+      <h4>June 2016 - March 2017</h4>
+
+      <div>
+        - Development of a voice chat module for Unity, using the C++ library "RakVoice", for a social VR game for 
+the GearVR platform.
+        - Development, of a mathematical puzzle game targeting mobile platforms.
+        - Participation in the development of a PAC-MAN clone advergame targeting Facebook.
+      </div>
+
+      <div class="tech-stack">Unity, C#, C++, RakNet RakVoice, HTML, Javascript, CSS</div>
+
+      <h3>Game Developer - Placeholder</h3>
+      <h4>April 2016 - June 2016</h4>
+
+      <div>Gameplay programming of a mobile puzzle game based on illusions (inspired by Monument Valley).</div>
+
+      <div class="tech-stack">Unity 5, C#</div>
 
       <h2>Skills</h2>
 
       <div class="skill-set">
-        <h3>Lethal</h3>
-        <SkillRate name="Handguns" :rate="5" />
-        <SkillRate name="Rifles" :rate="5" />
-        <SkillRate name="Shotguns" :rate="5" />
-        <SkillRate name="Machineguns" :rate="5" />
-        <SkillRate name="Missile launchers" :rate="5" />
-        <SkillRate name="Knives" :rate="5" />
-        <SkillRate name="Hand to hand combat" :rate="5" />
+        <h3>Languages</h3>
+        <div class="skill">C#</div>
+        <div class="skill">C++</div>
+        <div class="skill">PHP</div>
+        <div class="skill">Java</div>
+        <div class="skill">Javascript</div>
+        <div class="skill">Typescript</div>
+        <div class="skill">PHP</div>
+        <div class="skill">MySQL</div>
       </div>
 
       <div class="skill-set">
-        <h3>Non-lethal</h3>
-        <SkillRate name="Sneaking" :rate="1" />
-        <SkillRate name="Negotiation" :rate="1" />
-        <SkillRate name="Seduction" :rate="1" />
-        <SkillRate name="Sense of humor" :rate="2" />
+        <h3>Tools & Frameworks</h3>
+        <div class="skill">Unity</div>
+        <div class="skill">WPF</div>
+        <div class="skill">Visual Studio</div>
+        <div class="skill">Firebase</div>
+        <div class="skill">Zinject</div>
+        <div class="skill">Atavism</div>
+        <div class="skill">RakVoice (RakNet)</div>
+        <div class="skill">GIT</div>
+        <div class="skill">Trello</div>
+      </div>
+
+      <div class="skill-set">
+        <h3>Web & Fullstack</h3>
+        <div class="skill">HTML</div>
+        <div class="skill">CSS</div>
+        <div class="skill">Laravel</div>
+        <div class="skill">Typescript</div>
+        <div class="skill">PHP</div>
+        <div class="skill">MySQL</div>
       </div>
       <div style="clear:both" />
 
@@ -105,17 +149,6 @@
   </div>
 </template>
 
-<script lang="ts">
-import Vue from "vue";
-import SkillRate from "@/components/SkillRate.vue";
-
-export default Vue.extend({
-  name: "Resume",
-  components: {
-    SkillRate,
-  },
-});
-</script>
 
 <style scoped>
 h2 {
@@ -172,6 +205,13 @@ h4 {
   text-align: center;
 }
 
+.skill {
+   float: left;
+   border: 1px solid hsla(0,0%,86.3%,.44);
+   padding: 2px 10px;
+   margin: 5px;
+}
+
 @media only screen and (min-width: 620px){
   .paragraph {
     float: left;
@@ -185,7 +225,8 @@ h4 {
 
   .skill-set {
     float: left;
-    padding-right: 60px; 
+    padding-right: 30px; 
+    max-width: 230px!important;
   }
 }
 </style>
